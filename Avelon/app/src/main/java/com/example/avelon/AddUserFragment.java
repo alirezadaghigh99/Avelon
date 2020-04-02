487294a36f9b431812684b67a58bf0808a223741
alireza <alireza.daghigh1999@gmail.com>
2020-04-03 02:49:47 +0430

    first page is almost completed

--- Avelon/app/src/main/java/com/example/avelon/AddUserFragment.java
+++ Avelon/app/src/main/java/com/example/avelon/AddUserFragment.java
@@ -2,6 +2,7 @@ package com.example.avelon;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,42 +22,108 @@ import java.util.List;
 
 public class AddUserFragment extends Fragment {
     Button addUserButton;
+     private static final int MAX_PLAYER = 10;
+    private boolean isFinished = false;
+    private final int[] id = new int[1];
+    private Button nextPageButton;
+
     TextView firstUser;
     public int numberOfLines = 3;
+    EditText editText;
 
     private List<User> users;
     ConstraintLayout myLayout = null;
+
+    public List<User> getUsers() {
+        return users;
+    }
+
     @Nullable
     @Override
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_add_user , container , false);
+        return inflater.inflate(R.layout.fragment_add_user, container, false);
 
     }
-    GridLayout gridLayout;
-    Button button;
-    AddUserEditTextHandler addUserHandler;
+
+    private GridLayout gridLayout;
+    private Button button;
+    private AddUserEditTextHandler addUserHandler;
+
     @Override
     public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
-    gridLayout = view.findViewById(R.id.edit_text_container);
-    button = view.findViewById(R.id.add_user_button);
-        final int[] id = new int[1];
-        id[0] = 0;
-    button.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            addUserHandler = new AddUserEditTextHandler(getActivity());
-            gridLayout.addView(addUserHandler.userAdder(getActivity() , id[0]));
-            id[0] = id[0] + 1;
+        findView(view);
+        configure();
+        checkStatement(id[0]);
 
+        nextPageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (id[0]<4){
+                    Toast.makeText(getActivity() , "Not enough player",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    //TODO
+                }
+            }
+        });
 
-        }
-    });
 
     }
 
+    private void configure() {
+        if (isFinished)
+            return;
 
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!isFinished) {
+                    addUserHandler = new AddUserEditTextHandler(getActivity());
+
+                    if (id[0]==0||!isEmpty(editText) ) {
+
+
+                        editText = addUserHandler.userAdder(getActivity(), id[0]);
+
+                        gridLayout.addView(editText);
+                        id[0] = id[0] + 1;
+                        Log.d("tttta", "id is" + id[0]);
+                        checkStatement(id[0]);
+                    }
+
+                    else {
+                        Toast.makeText(getActivity(),"no name added" , Toast.LENGTH_LONG);
+                    }
+                }
+
+
+            }
+
+        });
     }
+
+    private void findView(View view) {
+        gridLayout = view.findViewById(R.id.edit_text_container);
+        button = view.findViewById(R.id.add_user_button);
+        nextPageButton = view.findViewById(R.id.go_to_game_button);
+    }
+
+    private void checkStatement(int id) {
+        if (id == MAX_PLAYER)
+            isFinished = true;
+        configure();
+    }
+    private boolean isEmpty(EditText etText) {
+        if (etText.getText().toString().trim().length() > 0)
+            return false;
+        String name = editText.getText().toString();
+        users.add(new User(name));
+        return true;
+    }
+
+
+}
 
 
 
